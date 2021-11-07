f52a7c0461b288c6f394b64ddeab6263dd908559
RadmirShkred <psitecton@gmail.com>
2021-11-07 19:41:44 +0300

    feat: add decision 5th task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+const mainDir = path.join(__dirname, 'styles');
+const copyDir = path.join(__dirname, 'project-dist');
+const copyFiles = path.join(copyDir, 'bundle.css');
+
+fs.rm(copyFiles, e => {
+  if (e) throw e;
+});
+
+fs.readdir(mainDir, {withFileTypes: true}, (e, files) => {
+  if (e) throw e;
+  for (const file of files) {
+    if (file.isFile() && path.extname(file.name).slice(1) === 'css') {
+      fs.readFile(path.join(mainDir, file.name), (e, data) => {
+        if (e) throw e;
+        fs.appendFile(copyFiles, data, (e) => {
+          if (e) throw e;
+        });
+      });
+    }
+  }
+});
