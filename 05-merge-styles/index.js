30ac4b598dab8578a8cd8aebb5bd1ec93ad5df28
RadmirShkred <psitecton@gmail.com>
2021-11-08 21:05:06 +0300

    fix: fix decision 5th task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -4,7 +4,11 @@ const mainDir = path.join(__dirname, 'styles');
 const copyDir = path.join(__dirname, 'project-dist');
 const copyFiles = path.join(copyDir, 'bundle.css');
 
-fs.rm(copyFiles, e => {
+fs.writeFile(copyFiles, '', e => {
+  if (e) throw e;
+});
+
+fs.unlink(copyFiles, e => {
   if (e) throw e;
 });
 
@@ -14,6 +18,9 @@ fs.readdir(mainDir, {withFileTypes: true}, (e, files) => {
     if (file.isFile() && path.extname(file.name).slice(1) === 'css') {
       fs.readFile(path.join(mainDir, file.name), (e, data) => {
         if (e) throw e;
+        fs.writeFile(copyFiles, '', e => {
+          if (e) throw e;
+        });
         fs.appendFile(copyFiles, data, (e) => {
           if (e) throw e;
         });
