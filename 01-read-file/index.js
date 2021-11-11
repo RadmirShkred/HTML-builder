33d492ffcc4a8aaf23409a04370f8c44e22bcce8
RadmirShkred <psitecton@gmail.com>
2021-11-11 14:02:05 +0300

    fix: fix 1st task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,12 +1,8 @@
+console.clear();
+
 const fs = require('fs');
 const path = require('path');
-
 const file = path.join(__dirname, 'text.txt');
 
-fs.readFile(file, function (err, data) {
-  if (err) {
-    return console.error(err);
-  }
-  console.log(data.toString());
-});
-
+const result = fs.createReadStream(file, 'utf8');
+result.on('data', chunk => process.stdout.write(chunk));
